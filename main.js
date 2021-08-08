5f197570ae47fdfc40fbbd240c1341ff6867f892
Anas <alnabale2016@gmail.com>
2021-08-08 12:33:52 +0300

    start level

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+console.log("hi");
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
